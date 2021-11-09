7e1ab58df849e43fbd9848a9ae8cddfdbdcca80c
yoonjiO <ohyescoding@gmail.com>
2021-11-09 15:14:29 +0900

    add: react-helmet

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import {Helmet} from "react-helmet";
 
 function App() {
   return (
     <div className="App">
+      <Helmet htmlAttributes={{ lang: 'ko'}}>
+        <title>??</title>
+      </Helmet>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
